293c3b4857d2550b2e473b553b806ab252ba04b9
Alon Blumenreich <alon@mail.com>
2018-03-27 18:13:26 +0300

    Minor change

--- trap.c
+++ trap.c
@@ -106,13 +106,15 @@ trap(struct trapframe *tf)
 
 
   #ifndef FCFS
-  // Force process to give up CPU on clock tick.
-  // If interrupts were on while locks held, would need to check nlock.
-  if(myproc() && myproc()->state == RUNNING &&
-     tf->trapno == T_IRQ0+IRQ_TIMER && myproc()->rContTime >= QUANTUM)
-      yield();
+    // Force process to give up CPU on clock tick.
+    // If interrupts were on while locks held, would need to check nlock.
+    if(myproc() && myproc()->state == RUNNING &&
+       tf->trapno == T_IRQ0+IRQ_TIMER && myproc()->rContTime >= QUANTUM)
+        yield();
 
   #endif
+
+    
   // Check if the process has been killed since we yielded
   if(myproc() && myproc()->killed && (tf->cs&3) == DPL_USER)
     exit();
